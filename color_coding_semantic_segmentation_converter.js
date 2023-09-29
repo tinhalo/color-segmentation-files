9b64ab773f09479c65719441c722e3827d7f7dd7
Steven Altsman <manchuwook@tinhalo.com>
2023-09-28 22:28:10 -0500

    Updated generator and a doc html

--- color_coding_semantic_segmentation_converter.js
+++ color_coding_semantic_segmentation_converter.js
@@ -3,6 +3,13 @@ const path = require('path');
 const colorNameList = require('color-name-list');
 const nearestColor = require('nearest-color');
 const anyp = require('anypalette');
+const hbs = require('handlebars');
+const { compile } = hbs;
+
+// load the template
+const swatchTemplate = fs.readFileSync(path.resolve(__dirname, 'swatch.handlebars'), 'utf-8');
+// compile the template
+const swatchTemplateCompiled = compile(swatchTemplate);
 
 // nearestColor need objects {name => hex} as input
 const colors = colorNameList.reduce((o, { name, hex }) => Object.assign(o, { [name]: hex }), {});
@@ -16,12 +23,8 @@ const headers = lines.shift().split('\t');
 const segments = lines.map((line, index) => {
     const currentLine = line.split('\t');
     const [idx, ratio, train, val, stuff, colorCodeRGB, colorCodeHex, name] = currentLine;
-    const nearestColorName = nearest(colorCodeHex);
 
-    // Round up or down the distance to the nearest color
-    const distant = Math.round(nearestColorName.distance * 100) / 100;
-
-    // Split the name by ; and trim the spaces, then return 2 first names if more than one
+    // Split the name by ; and trim the spaces
     const names = name.split(';').map(name => name.trim());
 
     // Split string of "(1,2,3)" to array of [0,1,0.5] using regex
@@ -37,8 +40,6 @@ segments.forEach(segment => {
     pallette.push(segment)
 });
 
-// segments.forEach(segment => console.log(segment));
-
 // List formats from anypalette
 // console.log(Object.entries(anyp.formats).filter(
 //     (k, v) => {
@@ -47,18 +48,46 @@ segments.forEach(segment => {
 //     }
 // ));
 
-const wp = anyp.writePalette(
+const aco = anyp.writePalette(
     pallette,
     anyp.formats.ADOBE_COLOR_SWATCH_PALETTE
 );
+fs.writeFileSync(path.resolve(__dirname, 'color_coding_semantic_segmentation.aco'), aco);
 
-// Create a file to receive the palette buffer
-fs.writeFileSync(path.resolve(__dirname, 'color_coding_semantic_segmentation.aco'), wp);
+const ase = anyp.writePalette(
+    pallette,
+    anyp.formats.ADOBE_SWATCH_EXCHANGE_PALETTE
+);
+fs.writeFileSync(path.resolve(__dirname, 'color_coding_semantic_segmentation.ase'), ase);
 
-const wpcss = anyp.writePalette(
+const css = anyp.writePalette(
     pallette,
     anyp.formats.CSS_VARIABLES
 );
+fs.writeFileSync(path.resolve(__dirname, 'color_coding_semantic_segmentation.css'), css);
 
-// Create a file to receive the palette buffer
-fs.writeFileSync(path.resolve(__dirname, 'color_coding_semantic_segmentation.css'), wpcss);
+// Create the html doc from lines.map
+const d = lines.map(line => {
+    const currentLine = line.split('\t');
+    const [idx, ratio, train, val, stuff, colorCodeRGB, colorCodeHex, name] = currentLine;
+    const nearestColorName = nearest(colorCodeHex);
+
+    // Round up or down the distance to the nearest color
+    const distant = Math.round(nearestColorName.distance * 100) / 100;
+
+    // Split the name by ; and trim the spaces
+    const contexts = name.split(';').map(name => name.trim());
+
+    // return hex code, color name, contexts
+    return {
+        colorCodeHex,
+        colorCodeName: `${nearestColorName.name} (${distant} distance)`,
+        contexts,
+    }
+})
+
+// render the template with the data
+const rendered = swatchTemplateCompiled({ context: d });
+// print the rendered template
+// console.log(rendered); 
+fs.writeFileSync(path.resolve(__dirname, 'swatch.html'), rendered);
